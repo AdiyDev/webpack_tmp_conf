70e0d4cdde6e0fee3c28303d78ee46d39669a4fa
Arkadiy B <adiy.work.mail@ya.ru>
2022-12-05 01:56:20 +0300

    feat(webpack config): add babel babel polifill how entry preset-env and export default plugin

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     open: true,
     hot: true
   },
-  entry: path.resolve(__dirname, 'src', 'index.js'),
+  entry: ['@babel/polyfill', path.resolve(__dirname, 'src', 'index.js')],
   output: {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
@@ -54,6 +54,18 @@ module.exports = {
           },
           'sass-loader'
         ]
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-export-default-from'],
+            ignore: ['node_modules', 'build']
+          }
+        }
       }
     ]
   }
